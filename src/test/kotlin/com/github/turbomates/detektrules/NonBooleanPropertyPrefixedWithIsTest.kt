a44d44111f000925b6523c5fb95da49a7c4cff86
Ivan Novikov <novikov.ivan.work@gmail.com>
2022-07-08 15:54:37 +0300

    added NonBooleanPropertyPrefixedWithIs custom rule

---
+++ src/test/kotlin/com/github/turbomates/detektrules/NonBooleanPropertyPrefixedWithIsTest.kt
@@ -0,0 +1,26 @@
+package com.github.turbomates.detektrules
+
+import io.gitlab.arturbosch.detekt.rules.KotlinCoreEnvironmentTest
+import io.gitlab.arturbosch.detekt.test.TestConfig
+import io.gitlab.arturbosch.detekt.test.compileAndLintWithContext
+import io.kotest.matchers.collections.shouldHaveSize
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.junit.jupiter.api.Test
+
+@KotlinCoreEnvironmentTest
+internal class NonBooleanPropertyPrefixedWithIsTest(private val env: KotlinCoreEnvironment) {
+    private val config = TestConfig(mapOf("excludedTypes" to listOf("BooleanWrapper")))
+    private val subject = NonBooleanPropertyPrefixedWithIs(config)
+
+    @Test
+    fun `excluded type ignored`() {
+        val code = """
+            import com.github.turbomates.detektrules.BooleanWrapper
+
+            data class O (var isDefault: BooleanWrapper)
+            data class BooleanWrapper(val value: Boolean)
+        """
+        val findings = subject.compileAndLintWithContext(env, code)
+        findings shouldHaveSize 0
+    }
+}
